95abc4c775b6e16c3db6d3cc60006336883ae663
Mohitnimbark12 <nimbarkmohit892@gmail.com>
2026-07-01 09:45:59 +0530

    Add files via upload

---
+++ session 1/app_use_cases.py
@@ -0,0 +1,8 @@
+print("1. Instagram")
+print("   Python can be used to analyze user engagement data and recommend content.\n")
+
+print("2. YouTube")
+print("   Python can be used to process video analytics and improve video recommendations.\n")
+
+print("3. Google Maps")
+print("   Python can be used to analyze traffic data and suggest the fastest routes.\n")
